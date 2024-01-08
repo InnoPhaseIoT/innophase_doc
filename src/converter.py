2b3a9bdf6419d43d5043d635af414fe1036d792e
ArshadForcebolt <arshadqureshi@forcbolt.com>
2024-01-08 15:25:28 +0530

    update converter.py

--- src/converter.py
+++ src/converter.py
@@ -2,6 +2,7 @@ import os
 import pypandoc
 import docx2txt
 from pathlib import Path
+from shutil import copyfile
 
 
 def remove_link(rst_file):
@@ -14,93 +15,39 @@ def remove_link(rst_file):
 
 
 def convert_docx_to_rst(docx_file: str, rst_file: str) -> None:
-    images_path = f"{os.path.dirname(rst_file)}/media"
     try:
-        if not os.path.exists(images_path):
-            os.makedirs(images_path)
         pypandoc.convert_file(docx_file, "rst", outputfile=rst_file)
-        _ = docx2txt.process(docx_file, images_path)
+        images_folder = os.path.join(os.path.dirname(rst_file), "media")
+        os.makedirs(images_folder, exist_ok=True)
+        _ = docx2txt.process(docx_file, images_folder)
     except Exception as err:
-        print("Error during conversion:")
+        print(f"Error during conversion of {docx_file}: {err}")
         raise err
 
 
-def create_folder_mapping(root_folder, recursive=True):
-    folder_mapping = {}
-
-    for dirpath, dirnames, filenames in os.walk(root_folder):
-
-        relative_path = os.path.relpath(dirpath, root_folder)
-        if relative_path == ".":
-            relative_path = ""
-
-        docx_files = [filename for filename in filenames if filename.endswith(".docx")]
-        if docx_files:
-            parent_folder_name = os.path.basename(relative_path)
-            if parent_folder_name not in folder_mapping:
-                folder_mapping[parent_folder_name] = []
-
-            folder_mapping[parent_folder_name].extend([os.path.join(dirpath, filename) for filename in docx_files])
-
-        if recursive:
-            for subdir in dirnames:
-                subdir_path = os.path.join(dirpath, subdir)
-                create_folder_mapping(subdir_path, recursive)
-
-    return folder_mapping
-
-
-def find_file_recursive(folder_path, target_extension='.docx'):
-    folder_tree = {}
-    for item in os.listdir(folder_path):
-        item_path = os.path.join(folder_path, item)
-        if os.path.isdir(item_path):
-            result = find_file_recursive(item_path, target_extension)
-            if result:
-                print(result)
-                return result
-        else:
-            if os.path.isfile(item_path) and item_path.endswith(target_extension):
-                folder_tree["folder_path"] = item_path
-                return folder_tree
-
-
-def folders_name(root_dir):
-    for foldername, subfolders, filenames in os.walk(root_dir):
-        break
-    return subfolders
-
-
-def create_development_environments_rst(rst_file_path, folder_name, rst_mapping):
-    with open('index.rst', 'a') as index_writer:
-        index_writer.write("\n\n")
-        index_writer.write(f"{folder_name}\n")
-        index_writer.write("========================\n\n")
-        index_writer.write(".. toctree::\n")
-        index_writer.write("   :maxdepth: 2\n")
-        index_writer.write("   :hidden:\n")
-        index_writer.write(f"   :caption: {folder_name}\n\n")
-        for filenames in rst_mapping[folder_name]:
-            rel_path = os.path.relpath(filenames, rst_folder)
-            index_writer.write(f"   {rel_path}\n")
-
-
 if __name__ == "__main__":
-    abc = Path(os.path.abspath(__file__)).parent.parent
-    docx_folder = f"{str(abc)}/doc_files"
-    rst_folder = f"{str(abc)}/docs"
-    folder_to_convert_list = folders_name(docx_folder)
+    abc = Path(__file__).parent.parent
+    docx_folder = abc / "doc_files"
+    rst_folder = abc / "docs"
 
-    for folder_to_convert in folder_to_convert_list:
-        rst_mapping = {}
-        folder_name = os.path.join(docx_folder, folder_to_convert)
-        folder_maps = create_folder_mapping(folder_name)
-        rst_mapping[folder_to_convert] = []
-        for folders, files in folder_maps.items():
-            folder_path = os.path.join(rst_folder, folder_to_convert, folders)
-            os.makedirs(folder_path, exist_ok=True)
-            for file in files:
-                file_name = os.path.basename(file)
-                path = os.path.splitext(file_name)[0] + '.rst'
-                rst_file_path = os.path.join(folder_path, path)
-                convert_docx_to_rst(file, rst_file_path)
+    for root, dirs, files in os.walk(docx_folder):
+        rel_path = Path(root).relative_to(docx_folder)
+        new_folder = rst_folder / rel_path
+        new_folder.mkdir(parents=True, exist_ok=True)
+
+        for file in files:
+            if file.endswith('.docx'):
+                docx_file = os.path.join(root, file)
+                rst_file = new_folder / (os.path.splitext(file)[0] + ".rst")
+                convert_docx_to_rst(docx_file, rst_file)
+
+                media_folder = new_folder / "media"  # Set media folder path
+                if os.path.exists(media_folder):
+                    for media_file in os.listdir(media_folder):
+                        src_media_file = os.path.join(media_folder, media_file)
+                        dst_media_folder = os.path.join(new_folder, "media")
+                        os.makedirs(dst_media_folder, exist_ok=True)
+                        dst_media_file = os.path.join(dst_media_folder, media_file)
+                        if src_media_file != dst_media_file:  # Check if source and destination are different
+                            copyfile(src_media_file, dst_media_file)
+                            remove_link(rst_file)
